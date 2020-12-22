7261946107b912112b7c130159bfe7a4b2fa648c
Hugo <hugo.cullinan@gmail.com>
2020-12-22 20:44:12 +1100

    navbar added

---
+++ water-app/src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Product',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact us',
+        url: '#',
+        cName: 'nav-links'
+    }
+
+]
